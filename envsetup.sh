cd84721f85d500c10b18b518719f52e3e7b61d11
viperusgit <viperusgit@gmail.com>
2017-03-04 09:45:54 -0500

    rebranding pt2
    Change-Id: I2c3fe153512a103568e5ddca23d88cc291434853

--- envsetup.sh
+++ envsetup.sh
@@ -602,7 +602,7 @@ function breakfast()
             if [ -z "$variant" ]; then
                 variant="userdebug"
             fi
-            lunch $target-$variant
+            lunch vanilla_$target-$variant
         fi
     fi
     return $?
