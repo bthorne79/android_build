0664da0fdb0df50410222d0f3286c5b7eafcee69
viperusgit <viperusgit@gmail.com>
2017-03-04 09:25:59 -0500

    fix vendor location
    Change-Id: I4b66255f7d79e59637187e0cdef88de512c0b6c6

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,14 +179,14 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
+ifneq ($(CUSTOM_BUILD),)
+all_product_configs := $(shell ls vendor/vanilla/products/$(CUSTOM_BUILD).mk)
+else
 ifneq ($(strip $(TARGET_BUILD_APPS)),)
 # An unbundled app build needs only the core product makefiles.
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(CUSTOM_BUILD),)
-    all_product_configs := $(shell ls vendor/custom/products/$(CUSTOM_BUILD).mk)
-  else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
